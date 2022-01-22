7ce7f41ce6df901ee875469161e438a963cc3d50
KZTahat <kztahat96@gmail.com>
2022-01-22 22:23:01 +0200

    Adding comments, favIcon and deleting extra files

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 "use strict";
+// The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
 
+// selecting all resources taps from the HTML file
 let taps = document.querySelectorAll(".resourcesButtons");
-let resourcesContainers = document.querySelectorAll('.recourceContent');
+// selecting all resources content containers from the HTNL file
+let resourcesContainers = document.querySelectorAll(".recourceContent");
 
+// traversing the taps to add event listeners on clicking
 taps.forEach((element, tapNumber) => {
   element.addEventListener("click", () => {
+    //   this callback function will be excuted each time one of the taps is clicked
     for (let index = 0; index < resourcesContainers.length; index++) {
       if (index == tapNumber) {
-        resourcesContainers[index].style.display = 'inline-block';
-        taps[index].style.borderBottom = '2px solid gray';
+        //   when the clicked taps index is the same of the container index display the container
+        //   and display a border bottom to teh tap itself
+        resourcesContainers[index].style.display = "inline-block";
+        taps[index].style.borderBottom = "2px solid gray";
       } else {
-        resourcesContainers[index].style.display = 'none';
-        taps[index].style.borderBottom = 'none';
+        //   after displaying the clicked taps container hide all the others
+        resourcesContainers[index].style.display = "none";
+        taps[index].style.borderBottom = "none";
       }
     }
   });
